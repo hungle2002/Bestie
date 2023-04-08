c6102cadfdc57755b78ae5c086dca50f60973fa5
Tan Le <tanlethanh8266@gmail.com>
2023-04-08 14:36:48 +0700

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'plugin:react/recommended',
+        'standard'
+    ],
+    overrides: [
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: [
+        'react',
+        'simple-import-sort'
+    ],
+    rules: {
+        indent: ['error', 4],
+        'no-unused-vars': ['warn'],
+        'react/react-in-jsx-scope': 'off',
+        semi: ['error', 'always'],
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'max-len': ['error', 100]
+    }
+};
