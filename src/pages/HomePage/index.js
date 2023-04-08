f2293f79a01fe009a0f796870779e6b9f8f04939
hnimtadd <minhdat15012002@gmail.com>
2023-04-09 00:00:50 +0700

    added notion component

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,9 +1,11 @@
+import NotionTodo from '../../components/notionTodo';
+
 function HomePage () {
     return (
-        <div className='px-48 flex flex-row w-full gap-12 h-[880px]'>
+        <div className='px-48 flex flex-row w-full gap-12 '>
             <div className='basis-3/5 flex flex-col gap-6'>
                 <div className='bg-orange-300 basis-6/12'>Chart</div>
-                <div className='bg-green-300 basis-6/12'>Notion</div>
+                <div className='bg-green-300 basis-6/12'><NotionTodo/></div>
             </div>
             <div className='basis-2/5 flex flex-col gap-6'>
                 <div className='bg-orange-300 basis-9/12'>Plan</div>
