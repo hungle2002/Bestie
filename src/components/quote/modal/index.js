f33415b2a8f05cdbc67de1c2b50f9785a3ee77d6
tpphuhungbp <tpphuhungbp2@gmail.com>
2023-04-09 01:42:42 +0700

    animodal

---
+++ src/components/quote/modal/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable multiline-ternary */
+/* eslint-disable max-len */
+import React, { useEffect, useState } from 'react';
+function Modal () {
+    const Quote = [
+        'Self-management is the ability to prioritize and manage your own goals, time, and resources effectively.\n - Brian Tracy',
+        "Workaholism is a tricky addiction because it's often rewarded in our society. But the truth is: overworking is a red flag for underliving.\n -Brené Brown",
+        'Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.\n  - Christian D. Larson'
+    ];
+
+    return (
+        <>
+aaaaaaaaaaaa
+        </>
+    );
+}
+
+export default Modal;
