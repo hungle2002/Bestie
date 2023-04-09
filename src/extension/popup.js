47e5b73dfb8bff03e94d51dc3d4f12410e8c29b8
Tan Le <tanlethanh8266@gmail.com>
2023-04-09 07:49:44 +0700

    Config react popup

--- src/extension/popup.js
+++ src/extension/popup.js
@@ -1,19 +1,54 @@
 /* eslint-disable no-undef */
-window.onload = () => {
-    const $startButton = document.querySelector('.start');
+import '../index.css';
 
-    $startButton.onclick = () => {
-        // Get active tab
-        chrome.tabs.query({
-            active: true,
-            currentWindow: true
-        }, (tabs) => {
-        // Send message to script file
-            chrome.tabs.sendMessage(
-                tabs[0].id,
-                { injectApp: true },
-                response => window.close()
-            );
-        });
-    };
+// import './build/content.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+// chrome.tabs.insertCSS({ file: './build/content.js' });
+
+// create a new link element
+const link = document.createElement('link');
+
+// set the rel, type, and href attributes
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = chrome.runtime.getURL('build/content.css');
+
+// append the link element to the head of the document
+document.head.appendChild(link);
+
+const App = () => {
+    return (
+        <div className="h-[600px] w-[400px] bg-pink-200">
+            <h1>Hello world</h1>
+        </div>
+
+    );
 };
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
+// Message Listener function
+// eslint-disable-next-line no-undef
+// chrome.runtime.onMessage.addListener((request, sender, response) => {
+//     // If message is injectApp
+//     if (request.injectApp) {
+//     // Inject our app to DOM and send response
+//         injectApp();
+//         response({
+//             startedExtension: true
+//         });
+//     }
+// });
+
+// function injectApp () {
+//     const newDiv = document.createElement('div');
+//     newDiv.setAttribute('id', 'chromeExtensionReactApp');
+//     document.body.appendChild(newDiv);
+//     ReactDOM.render(<App />, newDiv);
+//     console.log('Bestie: Content script');
+// }
